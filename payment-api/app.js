d535f1ce4aded710f449f87d9a79b05c8f47e02b
Adeyemi <adeyemiibrahim990@gmail.com>
2025-03-23 16:39:56 -0700

    fourth commit

--- payment-api/app.js
+++ payment-api/app.js
@@ -10,6 +10,11 @@ app.use(bodyParser.json());
 // Payment routes (with versioning)
 app.use('/api/v1/payments', paymentRoutes);
 
+// Add a root route to handle GET requests to '/'
+app.get('/', (req, res) => {
+  res.send('Welcome to the Payment API');
+});
+
 // Export the app for Vercel (as a serverless function)
 module.exports = (req, res) => {
   app(req, res);
